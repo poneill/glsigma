48afafd2ce0550c4d1d3e4a2f71b41a12e15fe1d
Patrick <synapse.and.syntax@gmail.com>
2014-02-04 17:59:55 -0500

    inverted energy matrix

--- src/rdoubledagger.py
+++ src/rdoubledagger.py
@@ -10,24 +10,27 @@ alphabet = 4
 epsilon = 2
 w = 16
 
+def find_Zb(G,w,epsilon):
+    return G*(((alphabet - 1)*1+exp(-beta*(-epsilon)))/alphabet)**w
 def rseq(G=10000,n=16,alpha=0.5,alpha_high=0.25,w=10,epsilon=2):
-    Zb = G*(((alphabet - 1)*exp(-beta*(epsilon))+1)/alphabet)**w
+    Zb = find_Zb(G,w,epsilon)
     Z = Zb/(1-alpha)
     e1 = (log(alpha_high) + log(Z))/-beta
     ei = (log(alpha-alpha_high) - log(n-1) + log(Z))/-beta
+    print "e1:",e1,"ei:",ei
     # check to see that probabilities are satisfied
     p_1 = exp(-beta*e1)/Z
     p_i = exp(-beta*ei)/Z
-    #print "alpha_high:",alpha_high,p_1
-    #print "alpha-alpha_high:",alpha-alpha_high,(n-1)*p_i
+    print "alpha_high:",alpha_high,p_1
+    print "alpha-alpha_high:",alpha-alpha_high,(n-1)*p_i
 
-    mismatches_1 = e1/(epsilon)
-    mismatches_i = ei/(epsilon)
-    #print "mismatches:",mismatches_1,mismatches_i
-    mismatch_prob1 = mismatches_1/w
-    mismatch_probi = mismatches_i/w
-    mismatch_prob = (mismatch_prob1 + (n-1)*mismatch_probi)/float(n)
-    match_prob = 1 - mismatch_prob
+    matches_1 = -e1/(epsilon)
+    matches_i = -ei/(epsilon)
+    print "matches:",matches_1,matches_i
+    match_prob1 = matches_1/w
+    match_probi = matches_i/w
+    match_prob = (match_prob1 + (n-1)*match_probi)/float(n)
+    mismatch_prob = 1 - match_prob
     column_entropy = -(match_prob*log2(match_prob) +
                       ((alphabet-1)*(mismatch_prob/(alphabet-1))*
                        log2(mismatch_prob/(alphabet-1))))
@@ -57,7 +60,7 @@ def new_stat(alphas,G=10000,w=16,epsilon=2):
     n = len(alphas)
     print "n:",n
     # Step 1: find ep_i for each alpha_i
-    Zb = G*(((alphabet - 1)*exp(-beta*epsilon)+1)/alphabet)**w
+    Zb = G*(((alphabet - 1)*1 + exp(-beta*-epsilon))/alphabet)**w
     print "Zb:",Zb
     alpha = sum(alphas)
     Z = Zb*(1/(1-alpha))
@@ -69,15 +72,22 @@ def new_stat(alphas,G=10000,w=16,epsilon=2):
     print "SSE:",sse
     assert sse < 1e-16
     # Step 2: compute Rseq from ep_is
-    qs = [ep/(epsilon*w) for ep in eps] # mismatch probabilities
-    print "qs:",qs
-    q = mean(qs) # average mismatch prob
-    p = 1 - q # match prob
+    ps = [-ep/(epsilon*w) for ep in eps] # match probabilities
+    print "ps:",ps
+    p = mean(ps) # average mismatch prob
+    pcheck = 1/(epsilon*beta)*(1.0/(n*w)*sum(log(a) for a in alphas) +
+                                (1.0/w)*(log(G)-log(1-alpha)) +
+                                log((3+exp(beta*epsilon))/4))
+    
+    
+    q = 1 - p # match prob
+    
+    print "match prob p:",p,"pcheck:",pcheck
     col_entropy = -(p*log2(p) + 3*(q/3.0)*log2(q/3.0))
     print "col_entropy:",col_entropy
     Hprior = log2(alphabet)
     rseq = w*(Hprior - col_entropy)
-    return rseq
+    return rseq,mean(eps),p
 
 def test_col_ent(m,w):
     q = m/float(w)
